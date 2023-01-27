af660287644585a7b78a83e571f5618efe732c97
Vlad Kurmaev <Vladikurmav@yandex.ru>
2023-01-27 17:12:53 +0500

    fix: deploy GH pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
   router: {
-    base: 'kurmaeva'
+    base: '/kurmaeva/'
   },
   head: {
     title: 'Юрист Курмаева М.В.',
